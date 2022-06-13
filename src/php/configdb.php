37bb35cfc1b4d12764ed374100ea6fd759f013cc
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-06-13 12:32:55 +0200

    Version servidor

--- src/php/configdb.php
+++ src/php/configdb.php
@@ -1,5 +1,9 @@
 <?php
-    define('HOSTNAME','localhost');
+    /* define('HOSTNAME','localhost');
     define('USERNAME','root');
     define('PASSWORD','');
-    define('DATABASE','LearningWords');
+    define('DATABASE','LearningWords'); */
+    define('HOSTNAME','18.2daw.esvirgua.com');
+    define('USERNAME','user2daw_18');
+    define('PASSWORD','x$FIK=b7qFQS');
+    define('DATABASE','user2daw_BD2-18');
--- src/views/creargame.php
+++ src/views/creargame.php
@@ -2,17 +2,22 @@
     require_once __DIR__. '/../php/controller/controller.php';
     $controlador=new Controller();
     $nombre=$_GET['nombre'];
-    echo '
-        <script>
-            window.onload=()=>{
-                let opcion=confirm("Antes de jugar deber crear una partida pulsando en Aceptar.")
-                if(opcion==true){
-                    '.$controlador->iniciarPartida($nombre).'
-                    window.location.href="./game.php?nombre='.$nombre.'&idPartida='.$controlador->modelo->conexion->insert_id.'";
-                }else{
-                    window.location.href="../index.html";
+    if(isset($nombre)){
+        echo '
+            <script>
+                window.onload=()=>{
+                    let opcion=confirm("Antes de jugar deber crear una partida pulsando en Aceptar.")
+                    if(opcion==true){
+                        '.$controlador->iniciarPartida($nombre).'
+                        window.location.href="./game.php?nombre='.$nombre.'&idPartida='.$controlador->modelo->conexion->insert_id.'";
+                    }else{
+                        window.location.href="../index.html";
+                    }
                 }
-            }
-        </script>
-    ';
+            </script>
+        ';
+    }else{
+        header("Location: ../index.html");
+    }
+    
 ?>
